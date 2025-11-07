b3f0be06b07f3584241e9c33eb93419ba135a865
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-11-07 10:46:31 +0400

    first commit

--- src/Components/Recips.jsx
+++ src/Components/Recips.jsx
@@ -6,6 +6,10 @@ import img from './images/Frame 15.png'
 import img2 from './images/Frame 16.png'
 import img3 from './images/Frame 17.png'
 import img4 from './images/Frame 18.png'
+import img5 from './images/Frame 21.png'
+import img6 from './images/Frame 23.png'
+import img7 from './images/Frame 24.png'
+import img8 from './images/Frame 22.png'
 export const Recips = () => {
   return (
     <div>
@@ -34,7 +38,35 @@ export const Recips = () => {
                     </a></li>
                 </ul>
             </div>
-    </div>
+
+                <div className="head_sec2">
+                    <h3>Check Out By Region !</h3>
+                </div>
+
+                <div className="rec_section2">
+                        <ul>
+                <li>
+                    <a href="">
+                      <img className='img' src={img5} alt="" />
+                      <p>Salads</p>
+                    </a>
+                </li>
+                <li><a href="">
+                    <img className='img'  src={img6} alt="" />
+                      <p>Appetizer</p>
+                    </a></li>
+                <li><a href="">
+                    <img className='img'  src={img7} alt="" />
+                      <p>Main Course</p>
+                    </a></li>
+                <li><a href="">
+                    <img className='img'  src={img8} alt="" />
+                      <p>Dessert</p>
+                    </a></li>
+                </ul>
+                </div>
+            </div>
+   
   )
 }
 
