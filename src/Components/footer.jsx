b891c874958d3b7905fb4e4eb153499adb537d0e
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-11-07 10:50:17 +0400

    first commit

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,1 +1,14 @@
 import css from './Footer.css'
+
+
+import React from 'react'
+
+export const footer = () => {
+  return (
+    <div>
+        <footer>
+            
+        </footer>
+    </div>
+  )
+}
