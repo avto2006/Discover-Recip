362f73c8deb88b8c3c273cf1aa4616640e92e476
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-11-08 13:17:32 +0400

    first commit

--- src/Components/AddMenu.jsx
+++ src/Components/AddMenu.jsx
@@ -1,8 +1,26 @@
 import css from './AddMenu.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddMenu = () => {
+
+    const [postRecipe,setPostRecip] = useState(0)
+    let text1 = document.querySelector('.text_ar2')
+    let text2 = document.querySelector('.text_ar3')
+
+    const handlePost = () => {
+        
+         if(text1 !== '' && text2 !== '') {
+         setPostRecip(alert('Post Send Succesfully'))
+    }   else if(text1 === '' && text2 !== '' ) {
+        setPostRecip(alert('Please enter the title'))
+    }   else if(text1 !== '' && text2 === '' ) {
+        setPostRecip(alert('Please enter Ingredients and Description'))
+    }   else {
+        setPostRecip(alert('Please fill in the fields'))
+    }
+}
+
   return (
     <div>
 
@@ -10,21 +28,19 @@ export const AddMenu = () => {
             <h3>Add The Recipe Of Your Choice !</h3>
 
             <div className="addMenuConteiner">
-                <textarea className='text_ar'>
-
-                </textarea>
+                <input className='inp' type="file" name="" id="" accept='image/*' />
                 <div className="upload">
                     upload photo 
                 </div>
                 <div className="title">
-                <textarea className='text_ar2'  name="" id="" placeholder='Title'></textarea>
+                <textarea  className='text_ar2'  name="" id="" placeholder='Title'></textarea>
                 </div>
 
                 <div className="ingredients">
-                <textarea placeholder='Ingredients & Description' className='text_ar3' name="" id=""></textarea>
+                <textarea  placeholder='Ingredients & Description' className='text_ar3' name="" id=""></textarea>
                 </div>
 
-                <div className="post">
+                <div className="post" onClick={handlePost}>
                     Post Recipe !!!
                 </div>
             </div>
